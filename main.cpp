5e1817c51ece72a0daa7f192a4c5d3a95bc43d38
azsxdcfvg <2513955283@qq.com>
2021-05-17 20:31:31 +0800

    更改测试

--- main.cpp
+++ main.cpp
@@ -2,10 +2,5 @@
 using namespace std;
 
 int main() {
-	ListNode* tail = new ListNode(3);
-	ListNode* mid = new ListNode(2, tail);
-	ListNode* head = new ListNode(1, mid);
-	output(head);
-	//output(removeNthFromEnd_my_brute(head, 3));
-	output(removeNthFromEnd_standard(head, 3));
+	
 }
